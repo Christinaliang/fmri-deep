71c1a76a5bbca8606545e8a910b87504cb374326
steven <stevenxcao@gmail.com>
2018-10-03 21:50:38 -0700

    Added more experiments

--- options.py
+++ options.py
@@ -18,6 +18,28 @@ def load_options(name):
         test = d.RestAtlasDataset('../data/test', tr)
         model = mp.PixelCNN
         optimizer = optim.Adam
+    if name == 'pixel2d_deep':
+        idx = d.read_idx('../data/Template_files/'+
+                         'Schaefer2018_400Parcels_7Networks_PCAordering.txt')
+        tr = t.Transforms((t.Reorder(0, slice(1, None, None)), 
+                           t.Reorder(0, idx),
+                           t.ChannelDim()))
+        # (img, lbl) is (1 x 1 x 400 x 200, 1 x 1 x 400 x 200)
+        train = d.RestAtlasDataset('../data/train', tr)
+        test = d.RestAtlasDataset('../data/test', tr)
+        model = lambda ch, shape: mp.PixelCNN(ch, shape, depth = 30)
+        optimizer = optim.Adam
+    if name == 'pixel2d_wide':
+        idx = d.read_idx('../data/Template_files/'+
+                         'Schaefer2018_400Parcels_7Networks_PCAordering.txt')
+        tr = t.Transforms((t.Reorder(0, slice(1, None, None)), 
+                           t.Reorder(0, idx),
+                           t.ChannelDim()))
+        # (img, lbl) is (1 x 1 x 400 x 200, 1 x 1 x 400 x 200)
+        train = d.RestAtlasDataset('../data/train', tr)
+        test = d.RestAtlasDataset('../data/test', tr)
+        model = lambda ch, shape: mp.PixelCNN(ch, shape, kernel = 5)
+        optimizer = optim.Adam
     example = train[0][0]
     ch, shape = example.shape[0], np.array(example.shape[1:])
     model = model(ch, shape)
