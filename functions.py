Fixed num stability issues, added input normalization

functions.py
@@ -1,25 +1,23 @@
 import numpy as np
 from scipy.interpolate import interp2d
 from skimage.measure import regionprops
-import torch
 import sys
 
 """
 Defines general utility functions
 """
-def softmax(X, axis):
-    y = np.atleast_2d(X)
-    y = y - np.expand_dims(np.max(y, axis = axis), axis)
-    y = np.exp(y)
-    ax_sum = np.expand_dims(np.sum(y, axis = axis), axis)
-    p = y / ax_sum
-    if len(X.shape) == 1: p = p.flatten()
-    return p
+def norm_sum(x):
+    if np.sum(x) == 0:
+        return x
+    return x / np.sum(x)
 
-def cycle_axes(tensor):
-    axes = np.arange(len(tensor.shape))
+def norm_01(x):
+    return (x - np.min(x)) / (np.max(x) - np.min(x))
+
+def cycle_axes(arr):
+    axes = np.arange(len(arr.shape))
     axes = np.roll(axes, 1)
-    return tensor.permute(int_tuple(axes))
+    return arr.permute(int_tuple(axes))
 
 def box(img, mask):
     bbox = regionprops(mask.astype(int), cache = False)[0].bbox
@@ -112,13 +110,6 @@ def apply_padf(d_in, d_out, kernel, stride, pad_f):
         stride = [stride for _ in range(len(d_in))]
     return np.array([pad_f(i, o, k, s) for i, o, k, s in 
                      zip(d_in, d_out, kernel, stride)])
-        
-def concat(a, b):
-    """Concatenates a and b on the channel dimension.
-    
-    Assumes a and b are B x C x ....
-    """
-    return torch.cat((a, b), 1)
 
 def tile_add(x, y):
     """Adds x and y with necessary tiling in the channel dimension.

model_mc.py
@@ -19,11 +19,12 @@ class Transition(b.Forward):
         
     def forward(self, x):
         """x should be the full time series."""
-        x = x.cycle_axes(x)
+        x = f.cycle_axes(x)
         frame = x[0]
-        video = frame
+        video = frame.unsqueeze(-1) # add time dim back in order to cat
         for t in range(x.shape[0] - 1):
-            video = np.concatenate((video, self.next_frame(frame)), axis = 0)
+            frame = self.next_frame(frame) # batch x ch x shape
+            video = torch.cat((video, frame.unsqueeze(-1)), -1) # b x c x s x t
             if self.steps == 'single':
                 frame = x[t + 1]
         return video
@@ -40,10 +41,18 @@ class MarkovLinear(nn.Module):
     """
     def __init__(self, size):
         super().__init__()
-        self.register_buffer('weights', torch.ones((size,size)))
+        print('P: ({}, {})'.format(size, size))
+        self.weight = nn.Parameter(torch.Tensor(size, size))
+        self.reset_parameters()
+        
+    def reset_parameters(self):
+        stdv = 1. / np.sqrt(self.weight.shape[1])
+        self.weight.data.uniform_(-stdv, stdv)
         
     def forward(self, x):
-        return F.linear(x, F.softmax(self.weights, dim = 1))
+        P = F.softmax(self.weight, dim = 1)
+        R = F.linear(x, P)
+        return R
         
 class MarkovChain(Transition):
     """Treats input vector as distribution over states of a markov chain and
@@ -51,6 +60,7 @@ class MarkovChain(Transition):
     """
     def __init__(self, ch, shape):
         super().__init__()
+        shape = np.roll(shape, 1)[1:] # remove time axis from shape
         size = f.size(shape) * ch
         self.P = MarkovLinear(size)
         self.criterion = nn.KLDivLoss()
@@ -63,5 +73,6 @@ class MarkovChain(Transition):
     
     def loss(self, output, label):
         """Assumes output and label are probabilities, applies KL div."""
-        output = torch.log(output) # modify to make numerically stable?
+        output = torch.clamp(output, min=1e-6)
+        output = torch.log(output)
         return self.criterion(output, label)

transform.py
@@ -29,13 +29,21 @@ class ToTensor(object):
     def __call__(self, arr):
         return torch.from_numpy(arr.copy()).float()
 
-class Softmax(object):
-    """Applies softmax to given dimension."""
+class NormalizeSum(object):
+    """Normalizes a given dimension to sum to 1."""
     def __init__(self, axis):
         self.axis = axis
         
     def __call__(self, arr):
-        return f.softmax(arr, self.axis)
+        return np.apply_along_axis(f.norm_sum, self.axis, arr)
+
+class Normalize01(object):
+    """Normalizes a given dimension to [0,1]."""
+    def __init__(self, axis):
+        self.axis = axis
+        
+    def __call__(self, arr):
+        return np.apply_along_axis(f.norm_01, self.axis, arr)
 
 class Reorder(object):
     """Reorders a dimension of the data according to indices"""